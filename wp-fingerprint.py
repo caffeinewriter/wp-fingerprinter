9da23493e490e46e94f66eb07a1e56d731b7184e
Brandon Anzaldi <brandon@brandonanzaldi.com>
2015-07-06 23:57:18 -0700

    :sparkles: Adding support for custom User Agents

--- wp-fingerprint.py
+++ wp-fingerprint.py
@@ -6,14 +6,15 @@ from urlparse import urlparse
 def main():
 	parser = argparse.ArgumentParser("Detects WP version from readme.html or meta tag")
 	parser.add_argument("-t", "--target", action="append", dest="targets", help="Add a target to scan")
+	parser.add_argument("-u", "--user-agent", action="store", dest="ua", help="A custom UA string to use in requests to the server")
 	parser.add_argument("-v", "--verbose", action="store_true", dest="verbose", help="Turn on verbose mode. Useful for seeing the details of errors")
 
 	args = parser.parse_args()
 
 	if args.targets != None:
-		scan(args.targets, args.verbose)
+		scan(args.targets, args.verbose, args.ua)
 
-def scan(targets, verbose):
+def scan(targets, verbose, ua):
 	for target in targets:
 		if not target[:5] == "http:" and not target[:6] == "https:":
 			target = "http://" + target
@@ -22,14 +23,20 @@ def scan(targets, verbose):
 		target = target + '/readme.html'
 		url = urlparse(target)
 		try:
-			r = requests.get(url.geturl())
+			if not ua:
+				r = requests.get(url.geturl())
+			else:
+				r = requests.get(url.geturl(), headers={'User-Agent': ua})
 		except requests.exceptions.ConnectionError, e:
 			print "Error: Unable to connect to " + tlurl.netloc
 			if verbose:
 				print e
 		else:
 			if r.status_code != 200:
-				t = requests.get(tlurl.geturl())
+				if not ua:
+					t = requests.get(tlurl.geturl())
+				else:
+					t = requests.get(tlurl.geturl(), headers={'User-Agent': ua})
 				w = re.compile(r'<meta name\=\"generator\" content\=\"(WordPress \d+\.\d+\.?(\d+)?)\" \/>', re.I)
 				n = w.search(t.text)
 				if n:
